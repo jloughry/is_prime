ec231aa384cda1834646de4be8fbb4c724f9034a
jloughry <joe.loughry@gmail.com>
2016-12-31 12:57:35 -0700

    Only check prine factors, for efficiency; report all factors.

--- Makefile
+++ Makefile
@@ -6,16 +6,26 @@ $(TARGET): $(source) Makefile
 	mv $@.exe $@
 
 clean:
-	rm -fv $(TARGET) a.exe
+	rm -fv $(TARGET) a.exe core
 
 vi:
 	/usr/bin/vim $(source)
 
+commit:
+	make clean
+	git add -A
+	git commit 
+
 test: $(TARGET) Makefile
-	@-./$(TARGET)
-	@-./$(TARGET) 0
-	@-./$(TARGET) -1
-	@-./$(TARGET) x
+	@echo Testing failure cases...
+	-./$(TARGET)
+	-./$(TARGET) 0
+	-./$(TARGET) -1
+	-./$(TARGET) x
+	@echo Testing hex and octal...
+	./$(TARGET) 0x10
+	./$(TARGET) 0x0777
+	@echo The following tests ought all to succeed...
 	@./$(TARGET) 1
 	@./$(TARGET) 2
 	@./$(TARGET) 3
@@ -26,5 +36,14 @@ test: $(TARGET) Makefile
 	@./$(TARGET) 8
 	@./$(TARGET) 9
 	@./$(TARGET) 10
+	@./$(TARGET) 100
+	@./$(TARGET) 210
+	@./$(TARGET) 2016
 	@./$(TARGET) 2017
-
+	@./$(TARGET) 2018
+	@./$(TARGET) 2019
+	@./$(TARGET) 2020
+	@./$(TARGET) 2021
+	@./$(TARGET) 2022
+	@./$(TARGET) 2023
+	@echo End of tests.
--- is_prime.c
+++ is_prime.c
@@ -1,8 +1,34 @@
+/*
+ * 2017 is a prime year; I wanted to check that.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
+#include <assert.h>
 
 #define USAGE "USAGE: %s n, where n is positive"
+
+bool prime (long n) {
+	assert (n > 0);
+	if (1 == n) {
+		return false;
+	}
+	if (n <= 3) {
+		return true;
+	}
+	if (n % 2 == 0) {
+		return false;
+	}
+	long limit = sqrt(n) + 1;
+	for (long factor = 3; factor < limit; factor += 2) {
+		if (n % factor == 0) {
+			return false;
+		}
+	}
+	return true;
+}
 
 int main(int argc, char ** argv) {
 	long n = 0;
@@ -19,30 +45,39 @@ int main(int argc, char ** argv) {
 		printf (USAGE "\n", argv[0]);
 		return EXIT_FAILURE;
 	}
-	if (n == 1) {
+	if (1 == n) {
 		printf ("1 is not prime\n");
 		return EXIT_SUCCESS;
 	}
-	if (n < 3) {
+	if (n <= 3) {
 		printf ("%ld is prime\n", n);
 		return EXIT_SUCCESS;
 	}
-	long limit = sqrt(n);
-	if (n % 2 == 0) {
-		printf ("%ld is divisible by 2\n", n);
+	long limit = sqrt(n) + 1;
+	long factor = 2;
+	bool is_prime = true;
+	bool we_already_said_divisible_by = false;
+	if (n % factor == 0) {
+		printf ("%ld is divisible by 2", n);
+		is_prime = false;
+		we_already_said_divisible_by = true;
 	}
-	else {
-		long factor;
-		for (factor = 3; factor <= limit; factor += 2) {
-			if (n % factor == 0) {
-				printf ("%ld is divisible by %ld\n", n, factor);
-				break;
+	for (factor = 3; factor <= limit && prime(factor); factor += 2) {
+		if (n % factor == 0) {
+			if (we_already_said_divisible_by) {
+				printf (" %ld", factor);
 			}
-		}
-		if (factor > limit) {
-			printf ("%ld is prime\n", n);
+			else {
+				printf ("%ld is divisible by %ld", n, factor);
+				we_already_said_divisible_by = true;
+			}
+			is_prime = false;
 		}
 	}
+	if (is_prime) {
+		printf ("%ld is prime", n);
+	}
+	printf ("\n");
 	return EXIT_SUCCESS;
 }
 
